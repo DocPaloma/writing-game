617e81158ff085f13b80eb5155b3fb05191ed8e1
Alejandro Medina <weaqla.xd@gmail.com>
2025-03-14 20:50:48 -0500

    major update

--- src/main/java/com/example/writinggame/controller/GameController.java
+++ src/main/java/com/example/writinggame/controller/GameController.java
@@ -1,12 +1,18 @@
 package com.example.writinggame.controller;
 
+import com.example.writinggame.model.Game;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class GameController {
 
@@ -23,7 +29,7 @@ public class GameController {
     private Label levelGameLabel;
 
     @FXML
-    private TextArea phraseToWriteTextArea;
+    private TextArea wordToWriteTextArea;
 
     @FXML
     private ImageView sunImageView;
@@ -34,9 +40,61 @@ public class GameController {
     @FXML
     private TextField writingWordTextField;
 
+    private Game game;
+    private int timeGame = 20;
+    private Timeline timeline;
+    private Stage wordWrittrn;
+
     @FXML
     void onActionCheckPhraseButton(ActionEvent event) {
 
+    }
+
+    public void initialize(){
+        game = new Game();
+        dogSaysTextArea.setDisable(true);
+        wordToWriteTextArea.setDisable(true);
+        wordToWriteTextArea.setText(game.getWord());
+        levelGameLabel.setText("Level: " + game.getLevelId());
+        loadDog();
+
+    }
+
+    private void startTimer(){
+        timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
+            if (timeGame > 0){
+                timeGame --;
+                timeGameLabel.setText("Tiempo: " + timeGame);
+            }
+            else {
+                timeline.stop();
+                endGame();
+            }
+        }));
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+    }
+
+    private void endGame(){
+        dogSaysTextArea.setText("¡Te has quedao sin tiempo!");
+        wordToWriteTextArea.setDisable(true);
+    }
+
+    private void loadDog(){
+        Image neutralCheems = new Image(getClass().getResourceAsStream("/com/example/writinggame/images/cheems.jpg"));
+        gameDogImageView.setImage(neutralCheems);
+    }
+
+    private void errorDog(){
+        Image neutralCheems = new Image(getClass().getResourceAsStream("/com/example/writinggame/images/cheems.jpg"));
+        Image error = new Image(getClass().getResourceAsStream("/com/example/writinggame/gifs/bonk-doge.gif"));
+        int seconds = 1;
+        gameDogImageView.setImage(error);
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(seconds), event -> {
+            gameDogImageView.setImage(neutralCheems);
+        }));
+        timeline.setCycleCount(1);
+        timeline.play();
     }
 
 }
--- src/main/java/com/example/writinggame/model/Game.java
+++ src/main/java/com/example/writinggame/model/Game.java
@@ -1,12 +1,127 @@
 package com.example.writinggame.model;
 
+/**
+ * Creation of Level class to do a simple level management
+ * @author Alejandro Medina
+ * @version 0.0.1
+ */
 public class Game {
-    public Level level;
-    public Dog dog;
+    /**
+     * The ID or number of a level
+     * @serialField
+     */
+    private int levelId;
+    /**
+     * The difficulty of a level
+     * @serialField
+     */
+    private String difficulty;
+    /**
+     * The word to be written
+     * @serialField
+     */
+    private String word;
 
+    private int levelsStreak;
+
+    private int maxStreak;
+
+    private int chances;
+
+    /**
+     * Creator method for a level.
+     * @param
+     */
     public Game() {
-        level = new Level();
-        dog = new Dog();
+        levelId = 1;
+        setDifficulty();
+        chooseWord();
+    }
+
+    /**
+     * Method that gets a level ID
+     * @return the ID of a level
+     */
+    public int getLevelId() {
+        return levelId;
+    }
+
+    /**
+     * Method that gets a level difficulty
+     * @return the difficulty of a level
+     */
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    /**
+     * Method that sets the difficulty of a level based on the level id
+     */
+    public void setDifficulty() {
+        if (levelId >= 1 && levelId <= 10) {
+            difficulty = "easy";
+        }
+        else if (levelId >= 11 && levelId <= 20) {
+            difficulty = "medium";
+        }
+        else if (levelId >= 21) {
+            difficulty = "hard";
+        }
+    }
+
+    /**
+     * Method that sets the Word to write
+     */
+    public void chooseWord () {
+
+        if (difficulty == "easy"){
+            String easyWords[]={};
+            int randomNumber = (int) (Math.random() * easyWords.length);
+            word = easyWords[randomNumber];
+
+        }
+
+        else if (difficulty == "hard"){
+            String hardWords[]={};
+            int randomNumber = (int) (Math.random() * hardWords.length);
+            word = hardWords[randomNumber];
+
+        }
+
+        else {
+            String mediumWords[] = {};
+            int randomNumber = (int) (Math.random() * mediumWords.length);
+            word = mediumWords[randomNumber];
+
+        }
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    /**
+     * Method that increases the level
+     * @param levelUP: variable that defines if the level needs to be increased
+     */
+    public void increaseLevelId(boolean levelUP) {
+        this.levelId = levelId + 1;
+    }
+
+    public void setLevelsStreak(Boolean a){
+        if (a==true){
+            levelsStreak = levelsStreak + 1;
+            if(levelsStreak>maxStreak){
+                levelsStreak = maxStreak;
+            }
+        }
+        else {
+            levelsStreak = 0;
+        }
 
     }
+
+
+
+
 }
